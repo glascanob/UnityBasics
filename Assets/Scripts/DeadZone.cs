a6b82ca5a4313c708d636edc63d85c2411f499c8
GabrielHome <gabriel@bitspacedevelopment.com>
2020-05-21 19:41:54 -0500

    Add DeadZones script and prefab

---
+++ Assets/Scripts/DeadZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            //Here assign the checkpoint
+            gameManager.RestartPlayer();
+        }
+    }
+}
