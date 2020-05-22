1b9bb3c918f517c4b18f47ee3017ccd2d46b1614
GabrielHome <gabriel@bitspacedevelopment.com>
2020-05-21 19:41:12 -0500

    Add Game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Checkpoint currentCheckpoint;
+    public PlayerController player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+    }
+
+    public void RestartPlayer()
+    {
+        player.dead = true;
+        player.transform.position = new Vector3(currentCheckpoint.transform.position.x, currentCheckpoint.transform.position.y + 1f, 0);
+        //wait maybe a couple of frames before letting it revive
+        player.dead = false;
+    }
+}
