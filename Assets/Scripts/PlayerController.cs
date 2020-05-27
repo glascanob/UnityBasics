609ae8fde2e98e161bd16dc676434a72f5f80b57
Gabriel <glascanob@gmail.com>
2020-05-26 21:16:45 -0500

    Added comments and headers to the code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Variables
+
     //Components of the PlayerController Class
     Rigidbody2D rb;
     Animator anim;
 
     //Variables that control our player
-    [SerializeField]
+    [Header("Player Settings")]
+    [SerializeField] // We use the SerializedField mainly to assing on inspector
     float speed;
     [SerializeField]
     float jumpForce;
 
-    //Other Variables
+    //Variables that help us recognize if the player is touching the ground
+    [Header("Ground Variables")]
     [SerializeField]
     Transform groundCheck;
 
@@ -25,23 +29,46 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     LayerMask ground;
     
+    /*Player state variables.
+     * This variables change depending on the player reaction to the level.
+     * They help us to understand what is happening to the player to apply
+     * animations, physics and other activities of the game flow.
+         */
     bool grounded;
+    [HideInInspector] //We don't need this variable to be shown on the inspector
     public bool dead = false;
+    #endregion
 
-    // Start is called before the first frame update
+    /* Start is called before the first frame update so here we can
+     * initialize our variables such as the rigidbody and the animator
+         */
     void Start()
     {
+        /*The GetComponent is a great tool Unity has to get access to one of the
+         * components a game object has. It can be used directly and by default
+         * it will try and get the component from the gameobject where the
+         * current script is added, or you can start with the name of the
+         * gameobject and then .GetComponent to get a component on another
+         * gameobject.
+         * Also you can use GetComponentInChildren or InParent to get components
+         * from children or parent gameobjects.
+         */
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
+    /*We are using this Unity function to draw a gizmo that help us configure
+     * the OverlapBox we create on the FixedUpdate
+         */ 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
 
         Gizmos.DrawCube(groundCheck.position, groundCheckSize);
     }
-    // Very usefull when talking about physics
+    /* Very usefull when talking about physics, that's why we use it to track if
+     * the player is touching the ground layer.
+         */
     private void FixedUpdate()
     {
         grounded = Physics2D.OverlapBox(groundCheck.position, groundCheckSize, 0, ground);
@@ -66,14 +93,18 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(-1f, 1f, 1f);
         }
     }
-
+    /*We created this function to update the animator variables with the
+     * information from the player activity
+     */ 
     void UpdateAnimator()
     {
         anim.SetBool("Grounded", grounded);
         anim.SetFloat("VSpeed", rb.velocity.y);
         anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
     }
-
+    /*Here we listen to control imputs from the user and run a function or
+     * code acordingly.
+     */ 
     void UpdateControls()
     {
         
