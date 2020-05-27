7dde26bdc4d087cc9bbb4784af81524ef15813b7
Gabriel <glascanob@gmail.com>
2020-05-26 21:17:12 -0500

    Added some explanation to the OnTriggerEnter2D function

--- Assets/Scripts/DeadZone.cs
+++ Assets/Scripts/DeadZone.cs
@@ -11,6 +11,11 @@ public class DeadZone : MonoBehaviour
         gameManager = FindObjectOfType<GameManager>();
     }
 
+    /* This Unity function let us track when ever another collider2D enters
+     * into a TRIGGER collider attached to this GameObject.
+     * IMPORTANT: For this to work at least one of the gameobjects needs to
+     * have a RigidBody, otherwise it won't track it.
+     */ 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
