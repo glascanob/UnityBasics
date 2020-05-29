1c99a57df7425613e973f511c8d37fb722b9349a
GabrielHome <gabriel@bitspacedevelopment.com>
2020-05-28 19:04:00 -0500

    Add Enemy Controller script
    Includes look at player, attack and dead state

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    #region Variables
+    //Game object components
+    Animator anim;
+
+    //Variables that control our enemy
+    public int health = 1;
+    bool dead = false;
+
+    //Other
+    Transform player;
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dead || player == null)
+            return;
+
+        if(player.position.x < transform.position.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    public void ReceiveDamage(int damageAmount)
+    {
+        if (dead)
+            return;
+        health -= damageAmount;
+
+        if(health <= 0)
+        {
+            dead = true;
+            anim.SetTrigger("Dead");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (dead)
+            return;
+        if(collision.CompareTag("Player"))
+        {
+            player = collision.transform;
+            anim.SetBool("Attack", true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (dead)
+            return;
+        if (collision.CompareTag("Player"))
+        {
+            player = null;
+            anim.SetBool("Attack", false);
+        }
+    }
+}
