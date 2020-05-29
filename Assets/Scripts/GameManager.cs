6b05fd207745fe3994eeeeb1379ebdb45464970b
GabrielHome <gabriel@bitspacedevelopment.com>
2020-05-28 20:13:00 -0500

    Implement Game pause menu with resume and restart.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
     public Checkpoint currentCheckpoint;
+    [HideInInspector]
     public PlayerController player;
+
+    public GameObject pauseButton;
+    public GameObject pauseMenu;
+
+    [HideInInspector]
+    public int points = 0;
 
     /* This is not the most efficent nor optimized way to use a GameManager
      * The FindObjectOfType is a powerfull but terrible for performance
@@ -19,7 +27,41 @@ public class GameManager : MonoBehaviour
         instance = this;
         player = FindObjectOfType<PlayerController>();
     }
+    private void Start()
+    {
+        pauseButton.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
 
+    public void PauseGame(bool pause)
+    {
+        pauseButton.SetActive(!pause);
+        pauseMenu.SetActive(pause);
+        /*
+         * Alternative way of doing the same as the following script line
+        if (pause)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+        */
+        Time.timeScale = pause ? 0 : 1;
+        player.gamePaused = pause;
+    }
+
+    public void AddPoints()
+    {
+        points ++;
+    }
+
+    public void RestartGame()
+    {
+        PauseGame(false);
+        SceneManager.LoadSceneAsync(0, LoadSceneMode.Single);
+    }
     public void RestartPlayer()
     {
         player.dead = true;
