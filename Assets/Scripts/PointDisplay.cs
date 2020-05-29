e774545933df7850beb14fcc379fa049900f68a5
GabrielHome <gabriel@bitspacedevelopment.com>
2020-05-28 20:12:33 -0500

    Add point display manager

---
+++ Assets/Scripts/PointDisplay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointDisplay : MonoBehaviour
+{
+    Text pointsText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pointsText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointsText.text = "" + GameManager.instance.points;   
+    }
+}
