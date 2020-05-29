d50cc286527eaae0d17e8459f1fd69a99ce59c2b
GabrielHome <gabriel@bitspacedevelopment.com>
2020-05-28 19:04:33 -0500

    Add Enemy Attack controller

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    #region Variables
+    PlayerController player;
+
+    public int damage = 1;
+    #endregion
+
+    public void DamageTarget()
+    {
+        if(player != null)
+        {
+            //Damage the player
+            player.ReceiveDamage(damage);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = collision.GetComponent<PlayerController>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = null;
+        }
+    }
+}
